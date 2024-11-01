5816310443e3e84428b2c600cf8c11e3619ab973
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-01 13:46:51 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const metadata = {
+  title: 'Call Dashboard',
+  description: 'Call Analytics Dashboard',
+}
+
 export default function RootLayout({
   children,
 }: {
